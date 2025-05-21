cc69b054811fd492e384dacbdd1c92802908f567
Marcos <terceiro.marcos2005@gmail.com>
2025-05-21 16:23:01 -0300

    apagando chamada de função

--- backend/gera_cobranca.js
+++ backend/gera_cobranca.js
@@ -119,8 +119,7 @@ async function gerarCobrancasDoDia() {
   }
 }
 
-// Executa imediatamente ao rodar o script
-gerarCobrancasDoDia();
+
 
 // Executa todos os dias às 01:00 da manhã
 cron.schedule("0 1 * * *", gerarCobrancasDoDia);
