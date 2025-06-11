2d7943c1c2ee45b584f2939c4250b79a864ae6e3
Marcos <terceiro.marcos2005@gmail.com>
2025-06-11 16:37:56 -0300

    refatoração do projeto - adicionando logs pra debug no service asaas_events

--- backend/controllers/webhookController.js
+++ backend/controllers/webhookController.js
@@ -7,8 +7,6 @@ async function receberEvento(req, res) {
     const event = req.body.event;
     const paymentId = req.body.payment.id;
 
-    console.log(paymentId);
-
     await webhookService.processarEvento(event, paymentId);
   } catch (err) {
     console.error("Erro no webhook:", err);
