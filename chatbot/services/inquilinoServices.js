323d1c7cd6359639de7e9688b2d98bb1eb60f8ca
Marcos <terceiro.marcos2005@gmail.com>
2025-06-13 14:35:08 -0300

    corrigindo função de buscar link de pagamento no chatbot

--- chatbot/services/inquilinoServices.js
+++ chatbot/services/inquilinoServices.js
@@ -18,6 +18,23 @@ const getInquilinoPorTelefone = async (numero) => {
   }
 };
 
+const buscarLinkPagamento = async (id) => {
+  try {
+    const response = await fetch(`${API_BASE}/pagamentos/link_pagamento/${id}`);
+
+    if (!response.ok) {
+      console.error("Nenhum inquilino encontrado");
+      return { erro: "Nenhum inquilino encontrado" };
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Erro ao buscar inquilino por telefone:", err);
+    return { erro: "Erro ao buscar inquilino por telefone" };
+  }
+};
+
 module.exports = {
   getInquilinoPorTelefone,
+  buscarLinkPagamento,
 };
