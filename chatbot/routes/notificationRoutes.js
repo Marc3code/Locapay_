55bdc2fb6fcf16122894d39ddf49ad5568f79bec
Marcos <terceiro.marcos2005@gmail.com>
2025-06-17 14:01:25 -0300

    ajustando rota para envio de notificações

--- chatbot/routes/notificationRoutes.js
+++ chatbot/routes/notificationRoutes.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
   res.send("Webhook para envio de notificações funcionando.");
 });
 
-// Rota de webhook
-router.post("notifications/cobrancaDoMes", notificationController.enviarNotificacaoCobrancadoMesController);
+// Rota para notificação de geração de cobrança do mes
+router.post("/cobranca_do_mes", notificationController.enviarNotificacaoCobrancadoMesController);
 
 module.exports = router;
--- chatbot/server.js
+++ chatbot/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/notifications', notificationRoutes);
+app.use("/notifications", notificationRoutes);
 app.use("/", webhookRoutes);
 
 const PORT = process.env.PORT || 3000;
