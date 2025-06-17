d140cf1308091dbb7cb6ba68fabddc26eb671deb
Marcos <terceiro.marcos2005@gmail.com>
2025-06-17 14:06:12 -0300

    ajustando rota para envio de notificações

--- chatbot/controllers/notificationsController.js
+++ chatbot/controllers/notificationsController.js
@@ -1,19 +1,24 @@
-const notificationService = require('../services/notificationsService');
+const notificationService = require("../services/notificationsService");
 
 const enviarNotificacaoCobrancadoMesController = async (req, res) => {
   try {
     const telefone = req.body.telefone;
     const data = req.body.data;
 
-    const envio = await notificationService.enviarNotificacaoCobrancadoMesService(data, telefone);
-    if(!envio.ok){
-        return ("Erro ao enviar notificação de cobrança do mês");
+    const envio =
+      await notificationService.enviarNotificacaoCobrancadoMesService(
+        data,
+        telefone
+      );
+    if (!envio.ok) {
+      return "Erro ao enviar notificação de cobrança do mês";
     }
+    return res.status(200).json({ message: "Notificação enviada com sucesso" });
   } catch (err) {
-    res.status(404).json({message: "erro ao enviar notificaçao de cobrança do mês"});
+    return { ok: false, error: err };
   }
 };
 
 module.exports = {
-    enviarNotificacaoCobrancadoMesController,
+  enviarNotificacaoCobrancadoMesController,
 };
