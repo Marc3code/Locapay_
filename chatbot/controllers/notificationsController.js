bbf3d14fcad3071da154ea78a474fb94dff313c6
Marcos <terceiro.marcos2005@gmail.com>
2025-06-17 14:08:50 -0300

    ajustando rota para envio de notificações > service e controller no backend

--- chatbot/controllers/notificationsController.js
+++ chatbot/controllers/notificationsController.js
@@ -1,24 +1,21 @@
-const notificationService = require("../services/notificationsService");
+const notificationService = require('../services/notificationsService');
 
 const enviarNotificacaoCobrancadoMesController = async (req, res) => {
   try {
     const telefone = req.body.telefone;
     const data = req.body.data;
 
-    const envio =
-      await notificationService.enviarNotificacaoCobrancadoMesService(
-        data,
-        telefone
-      );
-    if (!envio.ok) {
-      return "Erro ao enviar notificação de cobrança do mês";
+    const envio = await notificationService.enviarNotificacaoCobrancadoMesService(data, telefone);
+    if(!envio.ok){
+        return ("Erro ao enviar notificação de cobrança do mês");
     }
-    return res.status(200).json({ message: "Notificação enviada com sucesso" });
+      return res.status(200).json({ message: "Notificação enviada com sucesso" });
+
   } catch (err) {
-    return { ok: false, error: err };
+    res.status(404).json({message: "erro ao enviar notificaçao de cobrança do mês"});
   }
 };
 
 module.exports = {
-  enviarNotificacaoCobrancadoMesController,
+    enviarNotificacaoCobrancadoMesController,
 };
--- chatbot/services/notificationsService.js
+++ chatbot/services/notificationsService.js
@@ -1,17 +1,19 @@
-const notificacoes = require('../notifications/index')
+const notificacoes = require("../notifications/index");
 
 const enviarNotificacaoCobrancadoMesService = async (data, telefone) => {
   try {
-
-    const envio = await notificacoes.enviarNotificacaoCobrancaDoMes(data, telefone);
-    if(!envio.ok){
-        return ("Erro ao enviar notificação de cobrança do mês");
+    const envio = await notificacoes.enviarNotificacaoCobrancaDoMes(
+      data,
+      telefone
+    );
+    if (!envio.ok) {
+      return "Erro ao enviar notificação de cobrança do mês";
     }
   } catch (err) {
-    res.status(404).json({message: "erro ao enviar notificaçao de cobrança do mês"});
+    return { ok: false, error: err };
   }
 };
 
 module.exports = {
-    enviarNotificacaoCobrancadoMesService
-}
+  enviarNotificacaoCobrancadoMesService,
+};
