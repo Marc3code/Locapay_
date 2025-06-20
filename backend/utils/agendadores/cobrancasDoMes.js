e1305f5c590c1621687421775099eea193ed4e7d
Marcos <terceiro.marcos2005@gmail.com>
2025-06-20 15:14:47 -0300

    ajustando agendador cron

--- backend/utils/agendadores/cobrancasDoMes.js
+++ backend/utils/agendadores/cobrancasDoMes.js
@@ -2,5 +2,6 @@ const agendarTarefa = require("../agendarTarefa");
 const gerarCobrancasDoMes = require("../../tarefas/gerarCobrancasDoMes");
 
 // Roda no 1º dia do mês às 01:00
-agendarTarefa("*/2 * * * *", gerarCobrancasDoMes);
+agendarTarefa("0 17 * * *", gerarCobrancasDoMes);
+
 
