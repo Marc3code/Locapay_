872530bdefee602d37cbd04333c8e45032081793
Marcos <terceiro.marcos2005@gmail.com>
2025-06-21 13:30:04 -0300

    testando script de agendamento

--- backend/utils/agendadores/cobrancasDoMes.js
+++ backend/utils/agendadores/cobrancasDoMes.js
@@ -2,6 +2,6 @@ const agendarTarefa = require("../agendarTarefa");
 const gerarCobrancasDoMes = require("../../tarefas/gerarCobrancasDoMes");
 
 // Roda no 1º dia do mês às 01:00
-agendarTarefa("22 12 * * *", gerarCobrancasDoMes);
+agendarTarefa("31 14 * * *", gerarCobrancasDoMes);
 
 
