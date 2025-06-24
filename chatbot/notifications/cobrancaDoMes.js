259c112b839dd9b5120e24510f386ad81ee9d45b
Marcos <terceiro.marcos2005@gmail.com>
2025-06-24 16:56:04 -0300

    criando endpoint para envio de notificacoes de pagamento atrasado

--- chatbot/notifications/cobrancaDoMes.js
+++ chatbot/notifications/cobrancaDoMes.js
@@ -11,7 +11,7 @@ const enviarNotificacaoCobrancaDoMes = (data, telefone) => {
     .create({
       from: "whatsapp:" + FROM_NUMBER,
       to: "whatsapp:" + numeroFormatado,
-      body: `Sua fatura do aluguel com vencimento no dia ${dataFormatada} foi gerada e já está disponível para pagamento. Para pagar agora é só digitar 1.`,
+      body: `Sua fatura do aluguel com vencimento no dia ${dataFormatada} foi gerada e já está disponível para pagamento. Para pagar agora é só digitar 2.`,
     })
     .then((message) => {
       console.log("✅ Notificação de cobrança enviada:", message.sid);
