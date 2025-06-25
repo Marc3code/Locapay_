994900ca99c25928fa530636965184695c0ba1a3
Marcos <terceiro.marcos2005@gmail.com>
2025-06-25 15:36:27 -0300

    criando função envio de lembrete de pagamento > ajustando agendador para teste

--- backend/utils/agendadores/lembrete_pagamento.js
+++ backend/utils/agendadores/lembrete_pagamento.js
@@ -1,7 +1,7 @@
 const agendarTarefa = require("../agendarTarefa");
 const lembrete_pagamento = require("../../tarefas/lembrete_pagamento");
 
-// Roda todo dia
-agendarTarefa("02 * * * *", lembrete_pagamento);
+// Roda todo dia a cada 2 minutos
+agendarTarefa("*/2 * * * *", lembrete_pagamento);
 
 
