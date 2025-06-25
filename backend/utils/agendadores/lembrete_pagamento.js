7ba62511d4df6c6b88183292ea8750d10f2bee48
Marcos <terceiro.marcos2005@gmail.com>
2025-06-25 15:47:31 -0300

    criando função envio de lembrete de pagamento > atualizando agendador para todo dias as 12h

--- backend/utils/agendadores/lembrete_pagamento.js
+++ backend/utils/agendadores/lembrete_pagamento.js
@@ -1,7 +1,7 @@
 const agendarTarefa = require("../agendarTarefa");
 const lembrete_pagamento = require("../../tarefas/lembrete_pagamento");
 
-// Roda todo dia a cada 2 minutos
-agendarTarefa("*/2 * * * *", lembrete_pagamento);
+// Roda todo dia às 12h
+agendarTarefa("0 12 * * *", lembrete_pagamento);
 
 
