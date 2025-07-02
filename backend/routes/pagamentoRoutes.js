162417a609d9ed502c5e405355bebdd75381f64c
Marcos <terceiro.marcos2005@gmail.com>
2025-07-02 15:04:58 -0300

    adicionando endpoints para buscar pagamentos atrasados e pendentes

--- backend/routes/pagamentoRoutes.js
+++ backend/routes/pagamentoRoutes.js
@@ -5,7 +5,7 @@ const controller = require("../controllers/pagamentoController");
 router.get("/", controller.listarTodos);
 router.get("/link_pagamento/:inquilino_id", controller.buscarLinkPagamento);
 router.put("/updt_statusPagamento", controller.atualizarStatusPagamento);
-router.get("/atrasados/:id", controller.buscarPagamentosatrasados);
+router.get("/atrasados/:id", controller.buscarPagamentosAtrasados);
 router.get("/pendentes/:id", controller.buscarPagamentosPendentes)
 
 module.exports = router;
