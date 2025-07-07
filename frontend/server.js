ca8cd962b50e2272aa7801767fd982938bc9e970
Marcos <terceiro.marcos2005@gmail.com>
2025-07-07 16:52:19 -0300

    atualizando estrutura de pastas

--- frontend/server.js
+++ frontend/server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Define a pasta 'public' como estática
 app.use(express.static(path.join(__dirname, "public")));
 
-// Se quiser uma rota raiz para redirecionar
 app.get("/", (req, res) => {
   res.redirect("/login.html");
 });
