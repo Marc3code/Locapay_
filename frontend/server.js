18a4470bdc462f073ca8d431c06f1a6ce8731528
Marcos <terceiro.marcos2005@gmail.com>
2025-07-08 17:05:06 -0300

    corrigindo script do server

--- frontend/server.js
+++ frontend/server.js
@@ -7,19 +7,12 @@ const PORT = process.env.PORT || 3000;
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, "public")));
 
+
 // Redireciona a raiz para o login
 app.get("/", (req, res) => {
   res.redirect("/login.html");
 });
 
-// Para qualquer outra rota não tratada, tenta enviar o arquivo correspondente (útil para rotas como /components/header.html)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", req.path), (err) => {
-    if (err) {
-      res.status(404).send("Arquivo não encontrado");
-    }
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
