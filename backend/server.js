e08de3ca2ccb48baafb02a873f1ad7ad53dd0a8d
Marcos <terceiro.marcos2005@gmail.com>
2025-07-08 17:49:29 -0300

    autorizando requuisicoes do localhost

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5500",
+      "http://localhost:3000",
       "https://locapay-production.up.railway.app",
     ],
     methods: ["GET", "POST", "PUT"],
