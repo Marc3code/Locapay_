3e626fd8606e5dea5b6ac204e574cf0282a53d97
Marcos <terceiro.marcos2005@gmail.com>
2025-07-10 14:15:09 -0300

    criando primeiros endpoints

--- backend_assinaturas/server.js
+++ backend_assinaturas/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const assinaturaRoutes = require('./routes/assinaturasRoutes')
 
 
 app.use(cors()); 
@@ -14,6 +15,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('API funcionando!');
 });
+
+app.use(assinaturaRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
