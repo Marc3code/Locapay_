9785959e339c0007f617a50886a7cad2dc6d7297
Marcos <terceiro.marcos2005@gmail.com>
2025-07-10 16:36:13 -0300

    criando intermediação de acesso aos endpoints de manipulação de dados bancários

--- backend/services/dadosBancariosService.js
+++ backend/services/dadosBancariosService.js
@@ -2,7 +2,7 @@ const API_URL = process.env.API_BASE_ASSINATURAS;
 const API_KEY = process.env.ASSINATURAS_API_KEY;
 
 exports.enviarDados = async (locadorId, dados) => {
-  const url = `${API_URL}/dados-bancarios/${locadorId}`;
+  const url = `${API_URL}/bk-data/dados-bancarios/${locadorId}`;
 
   return await fetch(url, {
     method: 'POST',
@@ -15,7 +15,7 @@ exports.enviarDados = async (locadorId, dados) => {
 };
 
 exports.obterDados = async (locadorId) => {
-  const url = `${API_URL}/dados-bancarios/${locadorId}`;
+  const url = `${API_URL}/bk-data/dados-bancarios/${locadorId}`;
 
   return await fetch(url, {
     method: 'GET',
