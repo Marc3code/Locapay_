826565f2613c5e6507d33ec544f45ab74f9c60cd
Marcos <terceiro.marcos2005@gmail.com>
2025-07-14 16:15:41 -0300

    feita logica para criacao da linha de saldo após adição de dados bancários

--- backend/controllers/dadosBancariosController.js
+++ backend/controllers/dadosBancariosController.js
@@ -1,4 +1,4 @@
-const dadosBancariosService = require('../services/dadosBancariosService');
+const dadosBancariosService = require("../services/dadosBancariosService");
 
 exports.salvar = async (req, res) => {
   const id = req.userId;
@@ -6,7 +6,44 @@ exports.salvar = async (req, res) => {
 
   try {
     const resposta = await dadosBancariosService.enviarDados(id, dados);
-    return res.status(resposta.status).json(await resposta.json());
+
+    let respostaSaldoLocador = null;
+
+    try {
+      const resposta2 = await fetch(
+        `${process.env.API_BASE_ASSINATURAS}/saldos_locadores/adicionar`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.ASSINATURAS_API_KEY}`,
+          },
+          body: JSON.stringify({ locador_id: id }),
+        }
+      );
+
+      if (resposta2.ok) {
+        respostaSaldoLocador = await resposta2.json();
+      } else {
+        console.log(`Erro ao criar linha de saldo para o locador: ${id}`);
+        respostaSaldoLocador = {
+          error: `Erro ao criar linha de saldo para o locador: ${id}`,
+        };
+      }
+    } catch (err) {
+      console.error(err);
+      respostaSaldoLocador = {
+        error: "Erro ao comunicar com saldo do locador",
+        details: err.message,
+      };
+    }
+
+    const dadosResposta = await resposta.json();
+
+    return res.status(resposta.status).json({
+      dadosBancarios: dadosResposta,
+      saldoLocador: respostaSaldoLocador,
+    });
   } catch (err) {
     console.error("Erro ao salvar dados bancários remotamente:", err.message);
     return res.status(500).json({ erro: "Erro ao salvar dados bancários" });
