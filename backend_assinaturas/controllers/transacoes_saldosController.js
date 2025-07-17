c9ddfd749fbb10af87f0d5e7cd3dc5a74cffb26f
Marcos <terceiro.marcos2005@gmail.com>
2025-07-17 19:48:44 -0300

    removendo bug > estava fazendo uma atualização indevida no saldo do locador

--- backend_assinaturas/controllers/transacoes_saldosController.js
+++ backend_assinaturas/controllers/transacoes_saldosController.js
@@ -19,35 +19,6 @@ async function registrarTransacao(req, res) {
       descricao
     );
 
-    if (resultado.sucesso) {
-      try {
-        const response = await fetch(
-          "https://backendassinaturas-production.up.railway.app/saldos_locadores/atualizar",
-          {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.ASSINATURAS_API_KEY}`,
-            },
-            body: JSON.stringify({
-              locador_id: locador_id,
-              valor: valor,
-            }),
-          }
-        );
-
-        if (!response.ok) {
-          const erroTexto = await response.text();
-          console.warn(`Erro ao atualizar saldo do locador: ${erroTexto}`);
-        }
-      } catch (err) {
-        console.error(
-          "Erro de rede ao atualizar saldo do locador:",
-          err.message
-        );
-      }
-    }
-
     return res.status(200).json(resultado);
   } catch (error) {
     console.error("Erro no controller ao registrar transação:", error);
