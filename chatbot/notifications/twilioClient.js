64148ee1bfbd1f01ad8cf474a7bb6f2037fc7a8d
Marcos <terceiro.marcos2005@gmail.com>
2025-07-19 08:39:04 -0300

    adicionando comentario com template de mensagens

--- chatbot/notifications/twilioClient.js
+++ chatbot/notifications/twilioClient.js
@@ -5,3 +5,23 @@ const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 const FROM_NUMBER = process.env.TWILIO_NUMBER;
 
 module.exports = { client, FROM_NUMBER };
+
+
+/*return client.messages
+  .create({
+    from: "whatsapp:" + FROM_NUMBER,
+    to: "whatsapp:" + numeroFormatado,
+    template: {
+      name: "nome_do_template", // nome definido na sua conta Twilio
+      language: { code: "pt_BR" },
+      components: [
+        {
+          type: "body",
+          parameters: [
+            { type: "text", text: dataFormatada } // se o template tem {{1}}
+          ]
+        }
+      ]
+    }
+  })
+*/ 
