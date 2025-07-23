f9c1efef86f74e1789483a6d6c219c9cf69b613e
Marcos <terceiro.marcos2005@gmail.com>
2025-07-23 15:49:24 -0300

    adicionando endpoint para notificacoes do admin

--- chatbot/routes/admin/notificationRoutes.js
+++ chatbot/routes/admin/notificationRoutes.js
@@ -3,3 +3,5 @@ const router = express.Router();
 const notificationController = require("../../controllers/admin/notificationsController");
 
 router.post('/novo-user-cadastrado', notificationController.enviarNotificacaoNovoUsuario)
+
+module.exports = router;
